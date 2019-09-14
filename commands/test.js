c16b9c84bbff433ca12630b864a7abf7ac4dfe7f
Marethyu Dusk <maxischramm1@web.de>
2019-09-14 10:37:21 +0200

    test was successful
    yay, it worked!

--- commands/test.js
+++ commands/test.js
@@ -9,7 +9,7 @@ exports.config = {
 
 exports.condition = (client, channel, userstate, command, args, content) => {
 	if (command === "test" && client.config.admins.includes(userstate.username)) {
-		return true; //tmp comment
+		return true;
 	}
 	return false;
 };
