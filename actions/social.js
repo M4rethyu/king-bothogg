8a145bd16f090c6f560d27eab364a00ea9c4b45f
Marethyu Dusk <maxischramm1@web.de>
2019-10-02 21:52:19 +0200

    moved social to action

---
+++ actions/social.js
@@ -0,0 +1,12 @@
+exports.run = async (client) => {
+	client.twitch.linkSocialMedia("king_nidhogg");
+	return;
+};
+
+exports.config = {
+	"cooldown" : 45 * 60
+};
+
+exports.condition = (client) => {
+	return true;
+};
