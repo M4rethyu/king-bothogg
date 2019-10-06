b559e05514433a2a6133721e1a5fb2aa8057a132
Marethyu Dusk <maxischramm1@web.de>
2019-10-06 19:02:26 +0200

    add statistics

---
+++ discord/commands/statistics.js
@@ -0,0 +1,33 @@
+exports.run = async (client, message, permission, command, args, content) => {
+	
+	const map = client.persist("currency.amount");
+	//if (map == undefined) return false;
+	
+	var sum = 0;
+	const names = Object.getOwnPropertyNames(map);
+	names.forEach(name => {
+		//if (name == "king_nidhogg") return;
+		sum += map[name];
+	});
+	const n = names.length;
+	const avg = sum/n
+	
+	console.log(n)
+	console.log(sum)
+	console.log(avg)
+	
+	return;
+};
+
+exports.config = {
+	"cooldown" : 0,
+	"sharedCooldown" : true,
+	"permission" : 0
+};
+
+exports.condition = (client, message, permission, command, args, content) => {
+	if (command === "statistics") return true;
+	return false;
+};
+
+exports.help = "Statistics on nidcoins"
