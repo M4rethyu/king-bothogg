cc821402a4e4bcaf8c00e5668272c05efbe81228
Marethyu Dusk <maxischramm1@web.de>
2019-10-06 04:47:33 +0200

    Create update.js

---
+++ discord/commands/update.js
@@ -0,0 +1,30 @@
+exports.run = async (client, message, permission, command, args, content) => {
+	
+	client.log("warn", "UPDATING REPOSITORY");
+	
+	const exec = require("child_process").exec;
+	
+	exec("git pull", (err, stdout, stderr) => {
+		process.stdout.write(stdout)
+	});
+	setTimeout(function() {
+		exec("refresh", (err, stdout, stderr) => {
+			process.stdout.write(stdout)
+		});
+	}, 700);
+	
+	return;
+};
+
+exports.config = {
+	"cooldown" : 0,
+	"sharedCooldown" : true,
+	"permission" : 0
+};
+
+exports.condition = (client, message, permission, command, args, content) => {
+	if (command === "update" && client.config.hosted) return true;
+	return false;
+};
+
+exports.help = "Updates the repository."
