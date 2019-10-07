4520dc517552ab348a63cca62ef5e51adddcc9d2
Marethyu Dusk <maxischramm1@web.de>
2019-10-07 19:32:14 +0200

    move status change

--- actions/live.js
+++ actions/live.js
@@ -1,50 +1,9 @@
 exports.run = async (client) => {
-	const liveStati = [
-		(() => client.discord.user.setPresence({
-			game: {
-				name: "Erick's stream",
-				type: "WATCHING"
-			}
-		})),
-		(() => client.discord.user.setPresence({
-			game: {
-				name: "Erick's stream",
-				type: "LISTENING"
-			}
-		}))
-	];
 	
-	const deadStati = [
-		(() => client.discord.user.setPresence({
-			game: {
-				name: "you closely",
-				type: "WATCHING"
-			}
-		})),
-		(() => client.discord.user.setPresence({
-			game: {
-				name: "AI debates",
-				type: "LISTENING"
-			}
-		})),
-		(() => client.discord.user.setPresence({
-			game: {
-				name: "apocalyptic movies",
-				type: "WATCHING"
-			}
-		})),
-		(() => client.discord.user.setPresence({
-			game: {
-				name: "god",
-				type: "PLAYING"
-			}
-		}))
-	];
 	
 	const live = await client.twitch.live("king_nidhogg");
 	if (live) { // Is live
-		var i = Math.floor(Math.random()*liveStati.length);
-		liveStati[i]();
+		
 		
 		if (client.twitch.liveStatus) { // Was live => continued streaming
 			client.log("log", "Erick continued streaming");
@@ -52,8 +11,7 @@ exports.run = async (client) => {
 			client.log("log+", "Erick started streaming");
 		}
 	} else { // Isn't live
-		var i = Math.floor(Math.random()*deadStati.length);
-		deadStati[i]();
+		
 		
 		if (client.twitch.liveStatus) { // Was live => stopped streaming
 			client.log("log+", "Erick stopped streaming");
@@ -62,6 +20,7 @@ exports.run = async (client) => {
 		}
 	}
 	client.twitch.liveStatus = live; // Update live status for next time
+	client.actions.get("status").run(client);
 	return;
 };
 
---
+++ actions/status.js
@@ -0,0 +1,61 @@
+exports.run = async (client) => {
+	const liveStati = [
+		(() => client.discord.user.setPresence({
+			game: {
+				name: "Erick's stream",
+				type: "WATCHING"
+			}
+		})),
+		(() => client.discord.user.setPresence({
+			game: {
+				name: "Erick's stream",
+				type: "LISTENING"
+			}
+		}))
+	];
+	
+	const deadStati = [
+		(() => client.discord.user.setPresence({
+			game: {
+				name: "you closely",
+				type: "WATCHING"
+			}
+		})),
+		(() => client.discord.user.setPresence({
+			game: {
+				name: "AI debates",
+				type: "LISTENING"
+			}
+		})),
+		(() => client.discord.user.setPresence({
+			game: {
+				name: "apocalyptic movies",
+				type: "WATCHING"
+			}
+		})),
+		(() => client.discord.user.setPresence({
+			game: {
+				name: "god",
+				type: "PLAYING"
+			}
+		}))
+	];
+	
+	if (client.twitch.liveStatus) {
+		var i = Math.floor(Math.random()*liveStati.length);
+		liveStati[i]();
+	} else {
+		var i = Math.floor(Math.random()*deadStati.length);
+		deadStati[i]();
+	}
+	
+	return;
+};
+
+exports.config = {
+	"cooldown" : 0
+};
+
+exports.condition = (client) => {
+	return false;
+};
