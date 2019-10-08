5f38727292a0045d7ddee5d63559315d8a5bce51
Marethyu Dusk <maxischramm1@web.de>
2019-10-09 00:02:09 +0200

    Update coins.js

--- actions/coins.js
+++ actions/coins.js
@@ -1,13 +1,13 @@
 exports.run = async (client) => {
 	const live = await client.twitch.live("king_nidhogg")
-	if (live) {
+	if (true) {
 		client.log("log", "awarding chatters a nidcoin");
 		
 		const chatters = (await client.twitch.viewerlist("king_nidhogg")).chatters;
 		var list = chatters.broadcaster.concat(chatters.vips, chatters.moderators, chatters.staff, chatters.admins, chatters.global_mods, chatters.viewers);
 		
 		list.forEach(name => {
-			if client.twitch.config.bots.includes(name) return;
+			if (client.twitch.config.bots.includes(name)) return;
 			const amount = client.currency(name, 1);
 		});
 	}
