6ec2ee050d2d6586c51dedb29d193033d07ed285
Marethyu Dusk <maxischramm1@web.de>
2019-10-14 21:55:32 +0200

    rename

--- twitch/commands/add.js
+++ twitch/commands/add.js
@@ -6,36 +6,29 @@ exports.run = async (client, channel, userstate, command, args, content) => {
 		return false;
 	}
 	
-	var target = args[1]
+	const target = args[1].toLowerCase();
 	if (typeof target == "undefined") {
 		client.twitch.say(channel, "@" + userstate.username + ", please specify a valid target (!give [amount] [user])");
 		return false;
 	}
-	target = target.toLowerCase();
-	
-	if (client.currency(userstate.username) < amount) {
-		client.twitch.say(channel, "@" + userstate.username + ", you don't have enough " + client.answers.currencies);
-		return false;
-	}
 	
 	const before = client.currency(target);
-	client.currency(userstate.username, -amount);
 	client.currency(target, amount);
 	const after = client.currency(target);
 	
-	client.twitch.say(channel, "@" + target + ", " + userstate.username + " transferred " + amount + " " + ((amount == 1)?(client.answers.currency):(client.answers.currencies)) + " to you for a total of " + after);
+	client.twitch.say(channel, "@" + target + ", " + userstate.username + " gave you " + amount + " " + ((amount == 1)?(client.answers.currency):(client.answers.currencies)) + " for a total of " + after);
 	return;
 };
 
 exports.config = {
 	"cooldown" : 0,
 	"sharedCooldown" : false,
-	"permission" : 5
+	"permission" : 1
 };
 
 exports.condition = (client, channel, userstate, command, args, content) => {
-	if (command === "transfer") return true;
+	if (command === "add") return true;
 	return false;
 };
 
-exports.help = "!give [amount] [user] of money";
+exports.help = "!add [amount] [user] of money";
--- twitch/commands/give.js
+++ twitch/commands/give.js
@@ -6,24 +6,31 @@ exports.run = async (client, channel, userstate, command, args, content) => {
 		return false;
 	}
 	
-	const target = args[1].toLowerCase();
+	var target = args[1]
 	if (typeof target == "undefined") {
 		client.twitch.say(channel, "@" + userstate.username + ", please specify a valid target (!give [amount] [user])");
 		return false;
 	}
+	target = target.toLowerCase();
+	
+	if (client.currency(userstate.username) < amount) {
+		client.twitch.say(channel, "@" + userstate.username + ", you don't have enough " + client.answers.currencies);
+		return false;
+	}
 	
 	const before = client.currency(target);
+	client.currency(userstate.username, -amount);
 	client.currency(target, amount);
 	const after = client.currency(target);
 	
-	client.twitch.say(channel, "@" + target + ", " + userstate.username + " gave you " + amount + " " + ((amount == 1)?(client.answers.currency):(client.answers.currencies)) + " for a total of " + after);
+	client.twitch.say(channel, "@" + target + ", " + userstate.username + " transferred " + amount + " " + ((amount == 1)?(client.answers.currency):(client.answers.currencies)) + " to you for a total of " + after);
 	return;
 };
 
 exports.config = {
 	"cooldown" : 0,
 	"sharedCooldown" : false,
-	"permission" : 1
+	"permission" : 5
 };
 
 exports.condition = (client, channel, userstate, command, args, content) => {
