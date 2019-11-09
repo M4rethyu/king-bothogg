198d47507ee100cb8f5f2d25323fb5216e6c4ec5
Marethyu Dusk <maxischramm1@web.de>
2019-11-09 14:03:31 +0100

    Update cult.js
    permission

--- discord/ranks/cult.js
+++ discord/ranks/cult.js
@@ -2,6 +2,6 @@ exports.config = {
 	"abbreviation" : "cult",
 	"name" : "Church of Hogg",
 	"roleID" : 642704578386591747,
-	"permission" : 0,
+	"permission" : 5,
 	"help" : "The Church of Hogg is the following of The Great Hogg. Join us."
 };
