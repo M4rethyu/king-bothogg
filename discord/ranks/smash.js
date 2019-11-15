dcf6c6f14778166381f24f29a5b7d74ee3320acc
Marethyu Dusk <maxischramm1@web.de>
2019-11-15 20:30:26 +0100

    Update smash.js

--- discord/ranks/smash.js
+++ discord/ranks/smash.js
@@ -1,7 +1,7 @@
 exports.config = {
 	"abbreviation" : "sex",
 	"name" : "Smash",
-	"roleID" : 639670656652279813,
+	"roleID" : 644982383812149249,
 	"permission" : 5,
 	"help" : "slide into the DMs -> go on a date -> ~~play~~ smash"
 };
