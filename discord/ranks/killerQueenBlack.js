cfb4e479ea0f35df5ad45d00eeec5ba3d160c1a2
Marethyu Dusk <maxischramm1@web.de>
2019-11-15 18:42:53 +0100

    new ranks

---
+++ discord/ranks/killerQueenBlack.js
@@ -0,0 +1,7 @@
+exports.config = {
+	"abbreviation" : "kqb",
+	"name" : "Killer Queen Black",
+	"roleID" : 638962739112443904,
+	"permission" : 5,
+	"help" : "Nesnailssity cheeser strat is hidden op"
+};
---
+++ discord/ranks/smash.js
@@ -0,0 +1,7 @@
+exports.config = {
+	"abbreviation" : "sex",
+	"name" : "Smash",
+	"roleID" : 639670656652279813,
+	"permission" : 5,
+	"help" : "slide into the DMs -> go on a date -> ~~play~~ smash"
+};
