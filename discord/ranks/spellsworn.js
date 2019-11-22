185b00967cf73ce6ddaa4b5e0c1c18fcdb4f093b
Marethyu Dusk <maxischramm1@web.de>
2019-11-22 16:13:48 +0100

    Create spellsworn.js

---
+++ discord/ranks/spellsworn.js
@@ -0,0 +1,7 @@
+exports.config = {
+	"abbreviation" : "sps",
+	"name" : "Spellsworn",
+	"roleID" : 647145754682458123,
+	"permission" : 5,
+	"help" : "idk what this game does, someone give me a good description"
+};
