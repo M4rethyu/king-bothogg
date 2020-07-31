9d04c0773cc6ce6a43b3e4e4c64d7293093fd4d1
M4rethyu <42847769+M4rethyu@users.noreply.github.com>
2020-07-31 14:17:56 +0200

    debug daily.js

--- twitch/commands/daily.js
+++ twitch/commands/daily.js
@@ -1,10 +1,16 @@
 exports.run = async (client, message, channel, userstate, arguments, options) => {
 	const usedDaily = client.persist("currency.usedDaily." + userstate.username) || false;
 	if (usedDaily) return false;
-	
+
+	console.log(usedDaily)
+
 	const amount = Math.floor(Math.random()*21) + 10;
 	client.currency(userstate.username, amount);
-	
+
+	console.log(client.currency(userstate.username))
+	console.log(amount)
+	console.log(client.currency(userstate.username))
+
 	client.twitch.whisper(userstate.username, "You gained " + amount + " " + client.answers.currencies + " for a total of " + client.currency(userstate.username));
 	client.persist("currency.usedDaily." + userstate.username, true);
 	return true;
@@ -18,7 +24,7 @@ exports.config = {
 		
 	],
 	"channels" : "chat",
-	"help" : "A template for commands, so I can just copy paste"
+	"help" : "Hello everyone, this is YOUR daily dose of nidcoins"
 };
 
 exports.condition = (client, message, channel, userstate, arguments, options) => {
