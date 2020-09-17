ce22cbdf157581c03a3eef6ac863b200460a73c2
M4rethyu <42847769+M4rethyu@users.noreply.github.com>
2020-09-17 12:58:19 +0200

    enable greet message

--- discord/events/guildMemberAdd.js
+++ discord/events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = async (client, member) => {
-	//client.discord.arrivalChannel().send("Greetings, " + member + ". Welcome to **Nidhogg's Den** <:kingW:503777519607218197>");
+	client.discord.arrivalChannel().send("Greetings, " + member + ". Welcome to **Nidhogg's Den** <:kingW:503777519607218197>");
 
 	if (client.discord.lockdown) {
 		let raiderRole = client.discord.raiderRole();
