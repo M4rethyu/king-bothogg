36bee068371ddbc62dd2b5cbd88d8c38dfc4ee05
M4rethyu <42847769+M4rethyu@users.noreply.github.com>
2020-09-17 17:38:05 +0200

    add auto-lockdown when many people join

--- discord/events/guildMemberAdd.js
+++ discord/events/guildMemberAdd.js
@@ -1,8 +1,27 @@
+let recentlyJoined = []
+
 module.exports = async (client, member) => {
 	client.discord.arrivalChannel().send("Greetings, " + member + ". Welcome to **Nidhogg's Den** <:kingW:503777519607218197>");
 
+	let raiderRole = client.discord.raiderRole();
 	if (client.discord.lockdown) {
-		let raiderRole = client.discord.raiderRole();
 		await member.addRole(raiderRole.id);
+	}
+
+	let date = new Date();
+	let time = date.getTime();
+	recentlyJoined.push({"member" : member, "time" : time});
+	while (recentlyJoined.length > 5) {
+		recentlyJoined.shift();
+	}
+	if (recentlyJoined.length === 5) {
+		if (time - recentlyJoined[0]["time"] < 5000) {
+			client.discord.lockdown = true;
+			for (let member of recentlyJoined) {
+				member = member["member"];
+				await member.addRole(raiderRole.id);
+			}
+			client.discord.consoleChannel().send("<@&211329653250588672>, lockdown was enabled because 5 users joined within 5 seconds")
+		}
 	}
 };
