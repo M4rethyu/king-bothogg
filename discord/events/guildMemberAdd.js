6243f2c469d974f301dbb1ead5c189a88b9b93e2
M4rethyu <42847769+M4rethyu@users.noreply.github.com>
2020-10-04 12:51:21 +0200

    fix lockdown activating when it's already active

--- discord/events/guildMemberAdd.js
+++ discord/events/guildMemberAdd.js
@@ -4,24 +4,26 @@ module.exports = async (client, member) => {
 	client.discord.arrivalChannel().send("Greetings, " + member + ". Welcome to **Nidhogg's Den** <:kingW:503777519607218197>");
 
 	let raiderRole = client.discord.raiderRole();
-	if (client.discord.lockdown) {
+	if (client.discord.lockdown) { // Lockdown on
+		// Mark joining member as raider
 		await member.addRole(raiderRole.id);
-	}
-
-	let date = new Date();
-	let time = date.getTime();
-	recentlyJoined.push({"member" : member, "time" : time});
-	while (recentlyJoined.length > 5) {
-		recentlyJoined.shift();
-	}
-	if (recentlyJoined.length === 5) {
-		if (time - recentlyJoined[0]["time"] < 5000) {
-			client.discord.lockdown = true;
-			for (let member of recentlyJoined) {
-				member = member["member"];
-				await member.addRole(raiderRole.id);
+	} else { // Lockdown off
+		// Run code to test for Lockdown activation condition
+		let date = new Date();
+		let time = date.getTime();
+		recentlyJoined.push({"member": member, "time": time});
+		while (recentlyJoined.length > 5) {
+			recentlyJoined.shift();
+		}
+		if (recentlyJoined.length === 5) {
+			if (time - recentlyJoined[0]["time"] < 15000) {
+				client.discord.lockdown = true;
+				for (let member of recentlyJoined) {
+					member = member["member"];
+					await member.addRole(raiderRole.id);
+				}
+				client.discord.consoleChannel().send("<@&211329653250588672>, lockdown was enabled because 5 users joined within 15 seconds")
 			}
-			client.discord.consoleChannel().send("<@&211329653250588672>, lockdown was enabled because 5 users joined within 5 seconds")
 		}
 	}
 };
