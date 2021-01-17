c771d8335862bedec17117a49111e6a4fb42ba84
M4rethyu <42847769+M4rethyu@users.noreply.github.com>
2021-01-17 14:43:39 +0100

    change osu.js rank description

--- discord/ranks/osu.js
+++ discord/ranks/osu.js
@@ -3,5 +3,5 @@ exports.config = {
 	"name" : "cursed circle clickers",
 	"roleID" : 800356859398586468,
 	"permission" : 5,
-	"help" : "Evidently Erick execrates everyone enjoying expeditious euphonious entertainment"
+	"help" : "Evidently Erick execrates everyone enjoying expeditious euphonious exercise"
 };
