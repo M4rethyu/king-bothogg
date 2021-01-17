2ec48e2969b897fde150ff0dfb12068664d10ac0
M4rethyu <42847769+M4rethyu@users.noreply.github.com>
2021-01-17 14:33:12 +0100

    add osu.js rank

---
+++ discord/ranks/osu.js
@@ -0,0 +1,7 @@
+exports.config = {
+	"abbreviation" : "osu",
+	"name" : "cursed circle clickers",
+	"roleID" : 800356859398586468,
+	"permission" : 5,
+	"help" : "Evidently Erick execrates everyone enjoying expeditious euphonious entertainment"
+};
