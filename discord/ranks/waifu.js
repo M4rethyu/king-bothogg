934d7aa616ec936bb89c4ad4e79c46180eebc798
M4rethyu <42847769+M4rethyu@users.noreply.github.com>
2021-01-19 14:46:24 +0100

    add waifu.js rank

---
+++ discord/ranks/waifu.js
@@ -0,0 +1,7 @@
+exports.config = {
+	"abbreviation" : "waifu",
+	"name" : "Waifu preservation initiative",
+	"roleID" : 801084833945354282,
+	"permission" : 5,
+	"help" : "A group of people who is down to be pinged to secure a Mudae-roll someone really wants"
+};
