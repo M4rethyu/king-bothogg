bfc3d6cdf3bbdce2edee26727792322fb6d99f06
M4rethyu <42847769+M4rethyu@users.noreply.github.com>
2021-01-19 22:51:47 +0100

    add dont_starve.js rank

---
+++ discord/ranks/dont_starve.js
@@ -0,0 +1,7 @@
+exports.config = {
+	"abbreviation" : "ds",
+	"name" : "Don't Starve",
+	"roleID" : 801207206715719680,
+	"permission" : 5,
+	"help" : "The game's title can't tell you what to do, it's not your mom!"
+};
