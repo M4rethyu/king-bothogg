c473e7f3c3a84d1ba338d7bff820e5906a97e613
M4rethyu <42847769+M4rethyu@users.noreply.github.com>
2021-01-19 14:47:42 +0100

    update waifu.js rank

--- discord/ranks/waifu.js
+++ discord/ranks/waifu.js
@@ -3,5 +3,5 @@ exports.config = {
 	"name" : "Waifu preservation initiative",
 	"roleID" : 801084833945354282,
 	"permission" : 5,
-	"help" : "A group of people who is down to be pinged to secure a Mudae-roll someone really wants"
+	"help" : "A group of people who are down to be pinged to secure a Mudae-roll someone really wants (cause that's what heroes do)"
 };
