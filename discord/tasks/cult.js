f819a5b0f790504503181acf4d80899fab5d68ff
M4rethyu <42847769+M4rethyu@users.noreply.github.com>
2021-01-20 23:50:37 +0100

    changed cult message

--- discord/tasks/cult.js
+++ discord/tasks/cult.js
@@ -2,8 +2,8 @@ exports.run = async (client) => {
 	
 	const channel = client.discord.cultChannel();
 
-	channel.send("Dear Children of The Great Hogg: Here is your daily reminder, that **This is not a cult.** Thank you.");
-
+	//channel.send("Dear Children of The Great Hogg: Here is your daily reminder, that **This is not a cult.** Thank you.");
+	channel.send("Dear Children of The Great Hogg: Here is your daily reminder **to roll for waifus** as much as you can. Thank you.");
 	return;
 };
 
