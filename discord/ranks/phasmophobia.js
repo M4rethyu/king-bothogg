25751b2d17faeb942f7f1d7cb0798234e808e078
M4rethyu <42847769+M4rethyu@users.noreply.github.com>
2021-03-15 15:52:39 +0100

    add phasmophobia.js rank

---
+++ discord/ranks/phasmophobia.js
@@ -0,0 +1,7 @@
+exports.config = {
+	"abbreviation" : "ph",
+	"name" : "Phasmophobia",
+	"roleID" : 821032746423812166,
+	"permission" : 5,
+	"help" : "spoopy ghosts"
+};
--- discord/tasks/cult.js
+++ discord/tasks/cult.js
@@ -2,8 +2,8 @@ exports.run = async (client) => {
 	
 	const channel = client.discord.cultChannel();
 
-	//channel.send("Dear Children of The Great Hogg: Here is your daily reminder, that **this is not a cult.** Thank you.");
-	channel.send("Dear Children of The Great Hogg: Here is your very irregular reminder, that **this is not a cult.** Thank you.");
+	channel.send("Dear Children of The Great Hogg: Here is your daily reminder, that **this is not a cult.** Thank you.");
+	//channel.send("Dear Children of The Great Hogg: Here is your very irregular reminder, that **this is not a cult.** Thank you.");
 
 	return;
 };
